8950f77b3940ed350aa4d3f176f8a785ece61e9e
heavtek <heaventek13@gmail.com>
2025-12-23 23:51:09 +0300

    final update

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -8,13 +8,58 @@ class CityController extends Controller
 {
     private static array $cities = [
         ['id' => 1, 'name' => 'Addis Ababa'],
-        ['id' => 2, 'name' => 'Bahir Dar'],
+        ['id' => 2, 'name' => 'Adama'],
     ];
 
-    public function index()
+    public function getAllCites()
     {
         return response()->json(self::$cities);
     }
 
-   
+    public function getCityById($id)
+    {
+        $city = collect(self::$cities)->firstWhere('id', (int) $id);
+
+        if (! $city) {
+            return response()->json(['message' => 'City not found'], 404);
+        }
+
+        return response()->json($city);
+    }
+
+    public function store(Request $request)
+    {
+        $city = [
+            'id' => count(self::$cities) + 1,
+            'name' => $request->name,
+        ];
+
+        self::$cities[] = $city;
+
+        return response()->json($city, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        foreach (self::$cities as &$city) {
+            if ($city['id'] == $id) {
+                $city['name'] = $request->name;
+                return response()->json($city);
+            }
+        }
+
+        return response()->json(['message' => 'City not found'], 404);
+    }
+
+    public function deleteCity($id)
+    {
+        foreach (self::$cities as $key => $city) {
+            if ($city['id'] == $id) {
+                array_splice(self::$cities, $key, 1);
+                return response()->json(['message' => 'City deleted']);
+            }
+        }
+
+        return response()->json(['message' => 'City not found'], 404);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CityController;
 
-Route::get('/cities', [CityController::class, 'index']);
-
+Route::get('cities', [CityController::class, 'getAllCites']);
+Route::get('cities/{id}', [CityController::class, 'getCityById']);
+Route::post('cities', [CityController::class, 'store']);
+Route::put('cities/{id}', [CityController::class, 'update']);
+Route::delete('cities/{id}', [CityController::class, 'deleteCity']);
